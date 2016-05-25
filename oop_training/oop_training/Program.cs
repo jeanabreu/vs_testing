819c1ced7f2a48b162c624adede210640c1b4112
jmabreu <jmabreu@4001122-PC>
2016-05-25 19:31:34 -0400

    upd 25/05/2016

--- oop_training/oop_training/Program.cs
+++ oop_training/oop_training/Program.cs
@@ -9,29 +9,58 @@ namespace oop_training
 {
     class Program
     {
+        public void test()
+        {
+            Console.WriteLine("test");
+        } 
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Alumno:\n");
+            /* Console.WriteLine("Alumno:\n");
 
-            var alumno1 = new alumno()
+             var alumno1 = new alumno()
+             {
+                 Id = 1,
+                 name = "Jean",
+                 lastname = "Abreu",
+                 age = 23,
+                 email = "abreustudio@gmail.com",
+                 telephone = "8094191544"
+             };
+
+             var persona1 = alumno1;
+
+             */
+
+            int number = 7;
+
+            if (number > 5)
             {
-                Id = 1,
-                name = "Jean",
-                lastname = "Abreu",
-                age = 23,
-                email = "abreustudio@gmail.com",
-                telephone = "8094191544"
-            };
-
-            var persona1 = alumno1;
+                Console.WriteLine("cinco");
+            }
+            if (number > 8)
+            {
+                Console.WriteLine("ocho");
+            }
+            //
+            int x;
+            Console.WriteLine("Var x");
+            x = Convert.ToInt16(Console.ReadLine());
 
             
-
-            Console.WriteLine();
-
-
-
+            for (int i = 1; i < x; i++) 
+            {
+                test();
+            }
+             
             Console.Read();
+
+
+
+            while (true)
+            {
+
+            }
         }
     }
 }
