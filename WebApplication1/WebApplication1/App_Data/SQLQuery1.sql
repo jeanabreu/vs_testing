7e58b82f385112a42cb5c4a7a02e92e6fefd30a0
jmabreu <jmabreu@4001122-PC>
2016-07-21 17:19:19 -0400

    .

--- WebApplication1/WebApplication1/App_Data/SQLQuery1.sql
+++ WebApplication1/WebApplication1/App_Data/SQLQuery1.sql
@@ -2,21 +2,36 @@ USE BI_VENTAS
 SELECT  TOP 10 
 	--RFDCNO as Numero_De_Documento ,
 	CUNO as Cuenta_Numero,
+	STNO as Codigo_Tienda,
 	CUNM as Nombre_Cliente, 
-	STNO as Numero_Tienda,
+	CUADD1 as Direccion1,
+	CUADD2 as Direccion2,
+	CUADD3 as Direccion3,
+	CUCYST as Ciudad,
+	ORBYNM as Ordenado_Por,
+	OEPHN as Telefono,
+	SPADL2 as Email, 
+	EQMFSN as Serie_Equipo,
+	EQMFCD as Marca, 
+	EQMFMD as Modelo,
+	CUEQNO,
+	ARRNO as Numero_Arreglo,
 	WONO as Orden_Trabajo,
-	DCDTOT as Monto_Presupuestado,
+	PADAMT as Monto_Presupuestado_Sin_Imp,
+	(PADAMT*1.18) as Impuestos_ITBIS,
+	DCDTOT as Monto_Presupuestado_Con_Imp,
 	ENDT8 as Fecha_Act_Documento,
+	QUEXD8 as Fecha_Expiracion,
 	TMDY6,
 	USPFID as Usuario_Operador_DBS,
 	SLSREP as Usuario_Operador_Nombre,
-	CURIND as Moneda,
-	QUEXD8 as Fecha_Expiracion
+	CURIND as Moneda	
 
 FROM PCPCOHD0
-WHERE RFDCNO = '00e050293'
+WHERE RFDCNO = '00q289473'
 
-SELECT  TOP 10 * FROM PCPCOHD0 WHERE RFDCNO = '00e050293';
+SELECT  TOP 10 * FROM PCPCOHD0 WHERE RFDCNO = '00Q289473';
 
-EXEC('SELECT ENDT8 FROM LIBP16.PCPCOHD0 WHERE RFDCNO = ''00E050293''') AT AS400P
+EXEC('SELECT * FROM LIBP16.PCPCOHD0 WHERE RFDCNO = ''00E050293''') AT AS400P
 
+SELECT   * FROM PCPCOPD0 WHERE RFDCNO = '00e050293';
